02038d4dc8dbe61bbdb4f711e6c506d9bd942c50
Juan Carlos <juancafe2@gmail.com>
2016-12-05 20:38:16 -0800

    Created the channel component

---
+++ components/channels/Channel.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+class Channel extends React.Component {
+  onClick(e) {
+    e.preventDefault();
+    const {channel, setChannel} = this.props;
+    setChannel(channel);
+  }
+  render() {
+    const {channel} this.props;
+    return (
+      const  {channel} = this.props;
+      <li>
+        <a onClick={this.onClick.bind(this)}>
+          {channel.name}
+        </a>
+      </li>
+    );
+  }
+}
+
+Channel.propTypes = {
+  channel: React.Proptypes.object.isRequired,
+  setChannel: React.PropTypes.func.isRequired,
+}
+export default Channel;
